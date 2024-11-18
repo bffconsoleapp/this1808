faaa9203a05d335a2408da5efe511155a2375194
atosamit <amit.3.mishra@atos.net>
2024-11-18 20:28:34 +0530

    Initial commit with schema and resolver

---
+++ graphql/this1808Schema.js
@@ -0,0 +1,23 @@
+-schema
+type Vehicle {
+  name: String!
+  model: String!
+  yearOfManufacturing: String!
+}
+
+input VehicleInput {
+  name: String!
+  model: String!
+  yearOfManufacturing: String!
+}
+
+type Query {
+  getVehicle(name: String!): Vehicle
+  listVehicles: [Vehicle!]!
+}
+
+type Mutation {
+  addVehicle(input: VehicleInput!): Vehicle
+}
+
+#
--- index.js
+++ index.js
@@ -7,8 +7,8 @@ import routes from './routes/app.route.js'; // Importing routes
 import { ApolloServer } from 'apollo-server-express'; // Import from apollo-server-express
 
 // Import GraphQL type definitions and resolvers
-import demotypeDefs from './graphql/demo.js';
-import demoresolver from './graphql/demoresolver.js';
+import demotypeDefs from './graphql/this1808Schema.js';
+import demoresolver from './graphql/this1808Resolver.js';
 
 const app = express();
 app.use(express.json());
